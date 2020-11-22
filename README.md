e541a9f96006b32c97caf3c00f97dde7c1d4eb9d
Chris Quinn <3247139+irishquinn@users.noreply.github.com>
2020-11-21 21:20:44 -0500

    new
    new

---
+++ README.md
@@ -0,0 +1,3 @@
+# Alexa_YouTube Skill
+Play Youtube Music via Alexa Echo.
+Alexa_YouTube skill makes it easy to play music and playlist from YouTube.
---
+++ src/responses.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "en-US": {
+    "AND": " and",
+    "NO_RESULTS_FOUND": "{0} did not return any results on YouTube.",
+    "NO_VIDEO_QUERY_DEFINED": "Please describe in your request which titles you would like to play.",
+    "NO_PLIST_QUERY_DEFINED": "Please describe in your request which Playlist you would like to play.",
+    "NO_PLIST_FOUND": "No Playlist found.",
+    "NOW_PLAYING": "I am now playing {0}.",
+    "ADD_TO_PLAYLIST": "Song has been added to the PlayList {0}.",
+    "ALREADY_IN_PLAYLIST": "Song has already been added to PlayList {0}.",
+    "REMOVE_FROM_PLAYLIST": "Song was removed from the PlayList.",
+    "PLAYLIST_REMOVED": "PlayList {0} has been removed.",
+    "ALL_PLAYLISTS_REMOVED": "All PlayLists have been removed.",
+    "GET_ALL_PLAYLISTS": "The following playlists were found: {0}.",
+    "ONLY_NUMBER_PLIST_QUERY": "Only numbers are allowed as playlist names.",
+    "NOT_IN_PLAYLIST": "Song was not found in the PlayList.",
+    "NOTHING_TO_RESUME": "You are not playing anything currently.",
+    "NOTHING_TO_REPEAT": "You have not selected a video to play.",
+    "LOOP_ON_TRIGGERED": "I will repeat your {0} selection infinitely.",
+    "LOOP_OFF_TRIGGERED": "I will no longer repeat your {0} selection.",
+    "REPEAT_TRIGGERED": "I will repeat your {0} selection once.",
+    "HELP_TRIGGERED": "To use the YouTube skill, tell the skill to search for the video you want. Additionally, once the video is playing, you can tell Alexa to  pause, restart, or loop it."
+  }
+};
